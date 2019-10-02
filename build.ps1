d2f83b98580e0ed05f5252ef24748f7a9c0d39c4
Arun Mahapatra <pratikarun@gmail.com>
2019-10-02 14:39:49 +0530

    chore: use opencover reports for codecov

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 dotnet pack
 if ($?) {
-    dotnet test test/TestLogger.UnitTests/TestLogger.UnitTests.csproj -p:CollectCoverage=true
+    dotnet test test/TestLogger.UnitTests/TestLogger.UnitTests.csproj -p:CollectCoverage=true -p:CoverletOutputFormat=opencover
 }
 if ($?) {
     dotnet test test/TestLogger.AcceptanceTests/TestLogger.AcceptanceTests.csproj
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # vi: set tw=0
 
 dotnet pack &&\
-dotnet test test/TestLogger.UnitTests/TestLogger.UnitTests.csproj -p:CollectCoverage=true &&\
+dotnet test test/TestLogger.UnitTests/TestLogger.UnitTests.csproj -p:CollectCoverage=true -p:CoverletOutputFormat=opencover &&\
 dotnet test test/TestLogger.AcceptanceTests/TestLogger.AcceptanceTests.csproj
