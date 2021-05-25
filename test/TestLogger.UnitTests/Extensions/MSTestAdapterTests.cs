fe35a07c0c7a39b0c5d357a88afc0c06b1405c68
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2021-05-25 15:43:00 -0400

    Test placeholder

---
+++ test/TestLogger.UnitTests/Extensions/MSTestAdapterTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Spekt Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Spekt.TestLogger.UnitTests.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Spekt.TestLogger.Core;
+    using Spekt.TestLogger.Extensions;
+    using TestResult = Microsoft.VisualStudio.TestPlatform.ObjectModel.TestResult;
+
+    [TestClass]
+    public class MSTestAdapterTests
+    {
+        [TestMethod]
+        public void TransformShouldOverwriteMethodWithNonEmptyValues()
+        {
+            Assert.Fail("TODO");
+        }
+    }
+}
