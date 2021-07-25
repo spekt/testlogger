6c721fb33e11eb9b6f732e520e499e75d99947dc
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2021-07-25 13:56:48 -0400

    Test: Add simple test for mstest adapter

--- test/TestLogger.UnitTests/Extensions/MSTestAdapterTests.cs
+++ test/TestLogger.UnitTests/Extensions/MSTestAdapterTests.cs
@@ -16,10 +16,52 @@ namespace Spekt.TestLogger.UnitTests.Extensions
     [TestClass]
     public class MSTestAdapterTests
     {
+        private const string Method = "Method";
+
         [TestMethod]
         public void TransformShouldOverwriteMethodWithNonEmptyValues()
         {
-            Assert.Fail("TODO");
+            var param = Guid.NewGuid().ToString();
+            var result = new TestResult(new TestCase())
+            {
+                DisplayName = $"Method(\"{param}\")",
+            };
+
+            var testResults = new List<TestResultInfo>
+            {
+                new TestResultInfo(result, "namespace", "type", Method),
+            };
+
+            var sut = new MSTestAdapter();
+
+            var transformedResults = sut.TransformResults(testResults, new List<TestMessageInfo>());
+
+            Assert.AreEqual(1, transformedResults.Count);
+            Assert.IsTrue(transformedResults.Single().Method.Contains(param));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("    ")]
+        public void TransformShouldNoitOverwriteMethodEmptyValues(string displayName)
+        {
+            var result = new TestResult(new TestCase())
+            {
+                DisplayName = displayName,
+            };
+
+            var testResults = new List<TestResultInfo>
+            {
+                new TestResultInfo(result, "namespace", "type", Method),
+            };
+
+            var sut = new MSTestAdapter();
+
+            var transformedResults = sut.TransformResults(testResults, new List<TestMessageInfo>());
+
+            Assert.AreEqual(1, transformedResults.Count);
+            Assert.AreEqual(Method, transformedResults.Single().Method);
         }
     }
 }
