2cb7416d257a6076d3fe9ba78921039d2b594511
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-02-27 16:50:56 -0500

    Try custom scrubber for paths
    This somewhat mangles messages from the test frameworks, but that's not too important now.

--- test/TestLogger.AcceptanceTests/TestLoggerAcceptanceTests.cs
+++ test/TestLogger.AcceptanceTests/TestLoggerAcceptanceTests.cs
@@ -5,6 +5,7 @@ namespace TestLogger.AcceptanceTests
 {
     using System;
     using System.IO;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Newtonsoft.Json;
@@ -30,9 +31,25 @@ namespace TestLogger.AcceptanceTests
             settings.UseDirectory("Snapshots");
             settings.UseParameters(testAssembly);
 
+            // Make any paths uniform regardless of OS.
+            settings.ScrubLinesWithReplace(x =>
+            {
+                var options = RegexOptions.IgnoreCase | RegexOptions.Compiled;
+                var pathMatch = new Regex(@"^(.{0,}: )(.{0,}test[\/\\]assets[\/\\]Json\.TestLogger)(.{0,})$", options);
+                if (pathMatch.IsMatch(x))
+                {
+                    var m = pathMatch.Match(x);
+                    var prefix = m.Groups[1].Captures[0].Value.Replace('\\', '/');
+                    var pathForwardSlashes = m.Groups[3].Captures[0].Value.Replace('\\', '/');
+                    x = prefix + "test/assets/Json.TestLogger" + pathForwardSlashes;
+                }
+
+                return x;
+            });
+
             if (testAssembly.Contains(".XUnit.", StringComparison.OrdinalIgnoreCase))
             {
-                settings.ScrubLinesWithReplace(x => System.Text.RegularExpressions.Regex.Replace(x, @"\[xUnit\.net [0-9:.]{11,}\]", "[xUnit.net _Timestamp_]"));
+                settings.ScrubLinesWithReplace(x => Regex.Replace(x, @"\[xUnit\.net [0-9:.]{11,}\]", "[xUnit.net _Timestamp_]"));
             }
 
             DotnetTestFixture.Execute(testAssembly, "test-results.json");
