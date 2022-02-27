f8bb2603cb80f2e22eb5d96309e632b306c5bf7e
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-02-27 18:13:44 -0500

    Limit dotnet framework tests to windows

--- test/TestLogger.AcceptanceTests/TestLoggerAcceptanceTests.cs
+++ test/TestLogger.AcceptanceTests/TestLoggerAcceptanceTests.cs
@@ -5,12 +5,12 @@ namespace TestLogger.AcceptanceTests
 {
     using System;
     using System.IO;
+    using System.Runtime.InteropServices;
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Newtonsoft.Json;
     using VerifyMSTest;
-    using VerifyTests;
     using static Spekt.TestLogger.UnitTests.TestDoubles.JsonTestResultSerializer;
 
     [TestClass]
@@ -18,15 +18,31 @@ namespace TestLogger.AcceptanceTests
     {
         [TestMethod]
         [DataRow("Json.TestLogger.MStest.NetCore.Tests")]
-        [DataRow("Json.TestLogger.MStest.NetFull.Tests")]
-        [DataRow("Json.TestLogger.MStest.NetMulti.Tests")]
         [DataRow("Json.TestLogger.NUnit.NetCore.Tests")]
-        [DataRow("Json.TestLogger.NUnit.NetFull.Tests")]
-        [DataRow("Json.TestLogger.NUnit.NetMulti.Tests")]
         [DataRow("Json.TestLogger.XUnit.NetCore.Tests")]
-        [DataRow("Json.TestLogger.XUnit.NetFull.Tests")]
+        [DataRow("Json.TestLogger.MStest.NetMulti.Tests")]
+        [DataRow("Json.TestLogger.NUnit.NetMulti.Tests")]
         [DataRow("Json.TestLogger.XUnit.NetMulti.Tests")]
         public Task VerifyTestRunOutput(string testAssembly)
+        {
+            return this.VerifyAssembly(testAssembly);
+        }
+
+        [TestMethod]
+        [DataRow("Json.TestLogger.MStest.NetFull.Tests")]
+        [DataRow("Json.TestLogger.NUnit.NetFull.Tests")]
+        [DataRow("Json.TestLogger.XUnit.NetFull.Tests")]
+        public Task VerifyTestRunOutput_WindowsOnly(string testAssembly)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return Task.CompletedTask;
+            }
+
+            return this.VerifyAssembly(testAssembly);
+        }
+
+        private Task VerifyAssembly(string testAssembly)
         {
             var settings = new VerifyTests.VerifySettings();
             settings.UseDirectory("Snapshots");
