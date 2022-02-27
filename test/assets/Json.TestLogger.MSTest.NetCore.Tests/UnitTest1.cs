ad20387cfaed05f4ab1d3738766f0808104d9acc
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-02-27 15:55:48 -0500

    Add MSTest DataTestMethod

--- test/assets/Json.TestLogger.MSTest.NetCore.Tests/UnitTest1.cs
+++ test/assets/Json.TestLogger.MSTest.NetCore.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,6 +14,54 @@ namespace NUnit.Xml.TestLogger.NetFull.Tests
         public void ExampleFailure()
         {
             Assert.Fail();
+        }
+
+        [DataTestMethod]
+        [CustomDataSource]
+        public void Test_Add(int a, int b, int expected)
+        {
+            Assert.AreEqual(expected, a);
+            Assert.AreEqual(expected, b);
+        }
+    }
+
+    public class CustomDataSourceAttribute : Attribute, ITestDataSource
+    {
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            yield return new object[] { 1, 1, 2 };
+            yield return new object[] { 12, 30, 42 };
+            yield return new object[] { 14, 1, 15 };
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            if (data != null)
+                return string.Format(CultureInfo.InvariantCulture, "Custom - {0} ({1})", methodInfo.Name, string.Join(",", data));
+
+            return null;
+        }
+    }
+
+    [TestClass]
+    public class MathTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Property)]
+        public void Test_Add_DynamicData_Property(int a, int b, int expected)
+        {
+            Assert.AreEqual(expected, a);
+            Assert.AreEqual(expected, b);
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                yield return new object[] { 1, 1, 2 };
+                yield return new object[] { 12, 30, 42 };
+                yield return new object[] { 14, 1, 15 };
+            }
         }
     }
 }
