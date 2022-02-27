d8f5c7150e06455e40338f75c9bece613a277924
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2022-02-27 12:18:21 -0500

    Fix caching issue

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+dotnet nuget locals global-packages --clear
+Remove-Item .\test\package\bin\ -Recurse
+Remove-Item .\test\package\obj\ -Recurse
+
 dotnet pack
 if ($?) {
     dotnet test test/TestLogger.UnitTests/TestLogger.UnitTests.csproj -p:CollectCoverage=true -p:CoverletOutputFormat=opencover
