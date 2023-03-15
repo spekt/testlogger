ee352d44d4d2541dfe465547aa3a8e3ee1ec58e3
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2023-03-15 18:28:16 -0400

    Remove old xml sanitizer

--- src/TestLogger/Utilities/StringExtensions.cs
+++ src/TestLogger/Utilities/StringExtensions.cs
@@ -7,24 +7,6 @@ namespace Spekt.TestLogger.Utilities
 
      public static class StringExtensions
      {
-         public static string ReplaceInvalidXmlChar(this string str)
-         {
-             if (str != null)
-             {
-                 // From xml spec (http://www.w3.org/TR/xml/#charsets) valid chars:
-                 // #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
-                 // Following control charset are discouraged:
-                 // [#x7F-#x84], [#x86-#x9F], [#xFDD0-#xFDEF],
-                 // We are handling only #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD]
-                 // because C# support unicode character in range \u0000 to \uFFFF
-                 const string invalidChar = @"([^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD]|[\u007F-\u0084\u0086-\u009F\uFDD0-\uFDEF])";
-                 MatchEvaluator evaluator = ReplaceInvalidCharacterWithUniCodeEscapeSequence;
-                 return Regex.Replace(str, invalidChar, evaluator);
-             }
-
-             return str;
-         }
-
          public static string SubstringAfterDot(this string name)
          {
              if (string.IsNullOrEmpty(name))
@@ -55,12 +37,6 @@ namespace Spekt.TestLogger.Utilities
              }
 
              return string.Empty;
-         }
-
-         private static string ReplaceInvalidCharacterWithUniCodeEscapeSequence(Match match)
-         {
-             char x = match.Value[0];
-             return string.Format(@"\u{0:x4}", (ushort)x);
          }
      }
 }
