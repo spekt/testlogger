f5c713770974d26e483a64dd68a523cd4b494817
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2023-03-18 15:43:57 -0400

    Update equals and hashcode

--- src/TestLogger/Core/TestResultInfo.cs
+++ src/TestLogger/Core/TestResultInfo.cs
@@ -93,21 +93,47 @@ namespace Spekt.TestLogger.Core
 
         public string FullTypeName => this.Namespace + "." + this.Type;
 
+        public override bool Equals(object obj)
+        {
+            return obj is TestResultInfo info &&
+                   this.Namespace == info.Namespace &&
+                   this.Type == info.Type &&
+                   this.Method == info.Method &&
+                   this.Outcome == info.Outcome &&
+                   this.TestResultDisplayName == info.TestResultDisplayName &&
+                   this.TestCaseDisplayName == info.TestCaseDisplayName &&
+                   this.AssemblyPath == info.AssemblyPath &&
+                   this.CodeFilePath == info.CodeFilePath &&
+                   this.LineNumber == info.LineNumber &&
+                   this.StartTime == info.StartTime &&
+                   this.EndTime == info.EndTime &&
+                   this.Duration.Equals(info.Duration) &&
+                   this.ErrorMessage == info.ErrorMessage &&
+                   this.ErrorStackTrace == info.ErrorStackTrace &&
+                   EqualityComparer<List<TestResultMessage>>.Default.Equals(this.Messages, info.Messages) &&
+                   EqualityComparer<IReadOnlyCollection<Trait>>.Default.Equals(this.Traits, info.Traits) &&
+                   this.ExecutorUri == info.ExecutorUri &&
+                   this.FullTypeName == info.FullTypeName;
+        }
+
         public override int GetHashCode()
         {
-            int hashCode = -33026708;
+            int hashCode = -1082088776;
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.Namespace);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.Type);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.Method);
             hashCode = (hashCode * -1521134295) + this.Outcome.GetHashCode();
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.TestResultDisplayName);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.TestCaseDisplayName);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.AssemblyPath);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.CodeFilePath);
+            hashCode = (hashCode * -1521134295) + this.LineNumber.GetHashCode();
             hashCode = (hashCode * -1521134295) + this.StartTime.GetHashCode();
             hashCode = (hashCode * -1521134295) + this.EndTime.GetHashCode();
             hashCode = (hashCode * -1521134295) + this.Duration.GetHashCode();
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.ErrorMessage);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.ErrorStackTrace);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<IReadOnlyCollection<TestResultMessage>>.Default.GetHashCode(this.Messages);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<List<TestResultMessage>>.Default.GetHashCode(this.Messages);
             hashCode = (hashCode * -1521134295) + EqualityComparer<IReadOnlyCollection<Trait>>.Default.GetHashCode(this.Traits);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.ExecutorUri);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(this.FullTypeName);
