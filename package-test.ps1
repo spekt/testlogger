87deb9a7deac2c756052b2e40cf48c811afbc564
codito <codito@codito.in>
2025-02-12 22:00:09 +0530

    test: fix circleci test (#157)

--- package-test.ps1
+++ package-test.ps1
@@ -1,7 +1,6 @@
 Write-Host "Running package test..."
 dotnet pack
-dotnet build test/JUnit.Xml.TestLogger.AcceptanceTests/JUnit.Xml.TestLogger.AcceptanceTests.csproj
-dotnet test --no-build test/assets/JUnit.Xml.PackageTest/JUnit.Xml.PackageTest.csproj --logger:"junit;LogFilePath=TestResults/test-result.xml;FailureBodyFormat=Verbose;MethodFormat=Class"
+dotnet test test/assets/JUnit.Xml.PackageTest/JUnit.Xml.PackageTest.csproj --logger:"junit;LogFilePath=TestResults/test-result.xml;FailureBodyFormat=Verbose;MethodFormat=Class"
 
 Write-Host "-------------------"
 
