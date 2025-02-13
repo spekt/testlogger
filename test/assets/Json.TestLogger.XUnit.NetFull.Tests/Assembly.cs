656845f365905ac27bbb9da31fc579938d405433
Arun Mahapatra <codito@codito.in>
2025-02-13 20:53:14 +0530

    feat!: drop support for .NET full framework.

--- test/assets/Json.TestLogger.XUnit.NetFull.Tests/Assembly.cs
+++
@@ -1,6 +0,0 @@
-﻿
-// Added to ensure we get deterministic log message order for verification testing.
-// Example code from https://docs.microsoft.com/en-us/dotnet/core/testing/order-unit-tests?pivots=xunit
-[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
-[assembly: Xunit.TestCaseOrderer("Json.TestLogger.XUnit.Orderers.AlphabeticalOrderer", "Json.TestLogger.XUnit.NetFull.Tests")]
-[assembly: Xunit.TestCollectionOrderer("Json.TestLogger.XUnit.Orderers.DisplayNameOrderer", "Json.TestLogger.XUnit.NetFull.Tests")]
