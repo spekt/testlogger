0f4929b19daf3d0ff4c8e2530dd5f4fb4dc25e67
Arun Mahapatra <codito@codito.in>
2025-03-14 23:15:22 +0530

    fix: run netfull tests only on windows

--- test/JUnit.Xml.TestLogger.AcceptanceTests/JUnitTestLoggerNetFullAcceptanceTests.cs
+++ test/JUnit.Xml.TestLogger.AcceptanceTests/JUnitTestLoggerNetFullAcceptanceTests.cs
@@ -4,6 +4,7 @@
 namespace JUnit.Xml.TestLogger.AcceptanceTests
 {
     using System.IO;
+    using System.Runtime.InteropServices;
     using System.Xml.Linq;
     using global::TestLogger.Fixtures;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -20,18 +21,20 @@ namespace JUnit.Xml.TestLogger.AcceptanceTests
     {
         private const string AssetName = "JUnit.Xml.TestLogger.NetFull.Tests";
         private readonly string resultsFile;
-        private readonly XDocument resultsXml;
 
         public JUnitTestLoggerNetFullAcceptanceTests()
         {
             this.resultsFile = Path.Combine(AssetName.ToAssetDirectoryPath(), "test-results.xml");
-            this.resultsXml = XDocument.Load(this.resultsFile);
         }
 
         [ClassInitialize]
         public static void SuiteInitialize(TestContext context)
         {
-#if WINDOWS_OS
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return;
+            }
+
             var loggerArgs = "junit;LogFilePath=test-results.xml";
 
             // Enable reporting of internal properties in the adapter using runsettings
@@ -39,17 +42,19 @@ namespace JUnit.Xml.TestLogger.AcceptanceTests
                 .Create()
                 .WithBuild()
                 .Execute(AssetName, loggerArgs, collectCoverage: false, "test-results.xml");
-#endif
         }
 
         [TestMethod]
-        public void LoggedXmlValidatesAgainstXsdSchema()
+        public void NetFullLoggedXmlValidatesAgainstXsdSchema()
         {
-#if WINDOWS_OS
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return;
+            }
+
             var validator = new JunitXmlValidator();
             var result = validator.IsValid(File.ReadAllText(this.resultsFile));
             Assert.IsTrue(result);
-#endif
         }
     }
 }
